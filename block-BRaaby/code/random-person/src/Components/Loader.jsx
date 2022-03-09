fb3e43b4309212610983231d2e8ff75a8e86b6c1
manojkrishnak <manojkrishna11@gmail.com>
2022-03-10 01:58:39 +0530

    add files:react:Block-BRaaby

---
+++ block-BRaaby/code/random-person/src/Components/Loader.jsx
@@ -0,0 +1,17 @@
+import "./Loader.css"
+
+function Loader() {
+  return (
+    <>
+      <div class="bouncing-loader flex justify-ct align-ct">
+        <div></div>
+        <div></div>
+        <div></div>
+        <span>Loading ....</span>
+      </div>
+    </>
+  );
+}
+
+
+export default Loader;
